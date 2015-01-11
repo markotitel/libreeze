d79915c27e7ff2cad00ea1f9c837b986bed99315
Nikola Milinkovic <nmilinkovic@execom.eu>
2015-01-12 00:13:40 +0100

    WIP

--- app/views.py
+++ app/views.py
@@ -6,12 +6,7 @@ def index(request):
     return render(request, 'app/index.html')
 
 def submit_text(request):
-    # print request
-    print request.method
-    print request.POST
-    print request.REQUEST
-    item = request.POST.get('code')
-    print item
+    item = request.body
     return render(request, 'app/result.html', {'item': item})
 
 def submit_file(request):

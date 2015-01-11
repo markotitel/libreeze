c572a2b305b8642a3379dae612b6c4dce42c9c14
Nikola Milinkovic <nmilinkovic@execom.eu>
2015-01-11 19:07:03 +0100

    specified model

--- app/models.py
+++ app/models.py
@@ -2,16 +2,28 @@ from django.db import models
 
 # Create your models here.
 
+class Developer(models.Model):
+    email = models.EmailField
+
 class Project(models.Model):
+    POM = 'pm'
+    BOWER = 'bw'
+    SBT = 'sb'
+    GRADLE = 'gr'
+    PROJECT_TYPE_CHOICES = (
+        (POM, 'Java Maven'),
+        (BOWER, 'Javascript Bower'),
+        (SBT, 'Scala Build Tool'),
+        (GRADLE, 'Gradle')
+    )
+    owner = models.ForeignKey(Developer)
     name = models.CharField(max_length=512)
+    type = models.CharField(max_length=2, choices=PROJECT_TYPE_CHOICES, default=POM)
 
 class ProjectDependency(models.Model):
     project = models.ForeignKey(Project)
 
 class LatestDependency(models.Model):
-    group_id = models.CharField(max_length=512)
-    artifact_id = models.CharField(max_length=256)
+    key = models.CharField(max_length=1024).unique.db_index
     version = models.CharField(max_length=256).db_index
-    index_together = [group_id, artifact_id]
-    unique_together = (group_id, artifact_id, version)
 

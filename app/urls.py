a24a12bcc26a681308c248d2c5ac474c1289b97f
Nikola Milinkovic <nmilinkovic@execom.eu>
2015-01-12 00:10:26 +0100

    WIP

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import patterns, url
 from app import views
 
 urlpatterns = patterns('',
+    url(r'^submit_file$', views.submit_file, name='submit_file'),
+    url(r'^submit_text$', views.submit_text, name='submit_text'),
     url(r'^$', views.index, name='index'),
 )
--- app/views.py
+++ app/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the app index.")
+    return render(request, 'app/index.html')
+
+def submit_text(request):
+    # print request
+    print request.method
+    print request.POST
+    print request.REQUEST
+    item = request.POST.get('code')
+    print item
+    return render(request, 'app/result.html', {'item': item})
+
+def submit_file(request):
+    item = request.FILES.get('upload')
+    return render(request, 'app/result.html', { 'item': item})

8b22498ceaaea84e1dc867eae146d723454b0bcd
Nikola Milinkovic <nmilinkovic@execom.eu>
2015-01-11 21:38:59 +0100

    Generated DB

---
+++ app/migrations/0001_initial.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Developer',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('email', models.EmailField(max_length=75)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='LatestDependency',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('key', models.TextField(unique=True, max_length=1024, db_index=True)),
+                ('version', models.TextField(max_length=256, db_index=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Project',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.TextField(max_length=512)),
+                ('type', models.CharField(default=b'pm', max_length=2, choices=[(b'pm', b'Java Maven'), (b'bw', b'Javascript Bower'), (b'sb', b'Scala Build Tool'), (b'gr', b'Gradle')])),
+                ('owner', models.ForeignKey(to='app.Developer')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='ProjectDependency',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('version', models.TextField(max_length=256, blank=True)),
+                ('latest', models.ForeignKey(to='app.LatestDependency')),
+                ('project', models.ForeignKey(to='app.Project')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Developer(models.Model):
-    email = models.EmailField
+    email = models.EmailField()
 
 class Project(models.Model):
     POM = 'pm'
@@ -17,13 +17,14 @@ class Project(models.Model):
         (GRADLE, 'Gradle')
     )
     owner = models.ForeignKey(Developer)
-    name = models.CharField(max_length=512)
+    name = models.TextField(max_length=512)
     type = models.CharField(max_length=2, choices=PROJECT_TYPE_CHOICES, default=POM)
+
+class LatestDependency(models.Model):
+    key = models.TextField(max_length=1024, unique=True, db_index=True)
+    version = models.TextField(max_length=256, db_index=True)
 
 class ProjectDependency(models.Model):
     project = models.ForeignKey(Project)
-
-class LatestDependency(models.Model):
-    key = models.CharField(max_length=1024).unique.db_index
-    version = models.CharField(max_length=256).db_index
-
+    latest = models.ForeignKey(LatestDependency)
+    version = models.TextField(max_length=256, blank=True)

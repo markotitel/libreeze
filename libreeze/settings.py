f68101ae77fa625e95988c37413c013651690e41
Nikola Milinkovic <nikola1981@gmail.com>
2015-01-27 18:01:21 +0100

    Added settings for sqlite

--- libreeze/settings.py
+++ libreeze/settings.py
@@ -59,12 +59,15 @@ WSGI_APPLICATION = 'libreeze.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'libreeze',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3607',
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': 'libreeze',
+        # 'USER': 'root',
+        # 'PASSWORD': '',
+        # 'HOST': 'localhost',
+        # 'PORT': '3607',
+
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
